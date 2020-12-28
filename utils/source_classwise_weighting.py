6eef82dd08332e154da836683a6d38f1c25fe1e7
megh <megh.bhalerao@gmail.com>
2020-12-28 09:47:19 +0530

    bug fix wrt names

--- utils/source_classwise_weighting.py
+++ utils/source_classwise_weighting.py
@@ -11,11 +11,13 @@ def get_kNN(sim_distribution, feat_dict, k = 1):
     b_size = idxs.shape[0]
     k_neighbors = []
     labels_k_neighbors = []
+    names_k_neighbors = []
     for i in range(b_size):
         l = idxs[i].cpu().data.numpy()
         k_neighbors.append(list(l))
         labels_k_neighbors.append(list(np.array(feat_dict.labels)[l]))
-    return k_neighbors, labels_k_neighbors
+        names_k_neighbors.append(feat_dict.names[list(l)])
+    return k_neighbors, labels_k_neighbors, names_k_neighbors
 
 def make_feat_dict_from_idx(feat_dict,idxs):
     feat_dict_idx = edict({})
@@ -27,8 +29,8 @@ def make_feat_dict_from_idx(feat_dict,idxs):
 
 def get_k_farthest_neighbors(sim_distribution,feat_dict,K_farthest):
         sim_distribution.cosines = 1 - sim_distribution.cosines
-        k_farthest, labels_k_neighbors = get_kNN(sim_distribution, feat_dict, K_farthest)
-        return k_farthest, labels_k_neighbors
+        k_farthest, labels_k_farthest, names_k_farthest = get_kNN(sim_distribution, feat_dict, K_farthest)
+        return k_farthest, labels_k_farthest, names_k_farthest
 
 def do_source_weighting(loader, feat_dict,G,K_farthest,weight=0.8,aug = 2):
     farthest_classwise_examples_idx = []
@@ -40,10 +42,10 @@ def do_source_weighting(loader, feat_dict,G,K_farthest,weight=0.8,aug = 2):
         idxs_label = [i for i, x in enumerate(feat_dict.labels) if x == img_label]
         feat_dict_label = make_feat_dict_from_idx(feat_dict,idxs_label)
         f_batch, sim_distribution = get_similarity_distribution(feat_dict_label,batch,G)
-        k_farthest, labels_k_neighbors = get_k_farthest_neighbors(sim_distribution,feat_dict_label,K_farthest)
-        for farthest in k_farthest:
-            feat_dict.sample_weights[farthest] = weight
-
+        k_farthest, labels_k_farthest, names_k_farthest = get_k_farthest_neighbors(sim_distribution,feat_dict_label,K_farthest)
+        for name in names_k_farthest:
+            idx_to_weigh = feat_dict.names.index(name)
+            feat_dict.sample_weights[idx_to_weigh] = weight
             
 
 
